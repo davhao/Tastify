05f4e8886c39ecd7de4e1513b3a99ca6c8812b3b
David Hao <djhao@live.unc.edu>
2020-08-06 23:08:32 -0400

    fixed api calls to use proxy

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports = {
 	mongoURI      : process.env.mongoURI,
 	client_id     : process.env.client_id,
 	client_secret : process.env.client_secret,
-	redirect_uri  : process.env.redirect_uri,
+	redirect_uri  : `${process.env.PATH}/api/login/callback`,
 	scopes        : process.env.scopes
 };
