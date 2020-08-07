1087de98de6dce6034913fc0e19c41a228758a5d
David Hao <djhao@live.unc.edu>
2020-08-06 22:28:30 -0400

    readded keys file with process.env

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+	mongoURI      : process.env.mongoURI,
+	client_id     : process.env.client_id,
+	client_secret : process.env.client_secret,
+	redirect_uri  : process.env.redirect_uri,
+	scopes        : process.env.scopes
+};
