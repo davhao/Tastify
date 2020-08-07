a6b9702d1833fc9e390af1ef988672f58e3d5a39
David Hao <djhao@live.unc.edu>
2020-08-06 23:14:52 -0400

    changed redirect URI

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports = {
 	mongoURI      : process.env.mongoURI,
 	client_id     : process.env.client_id,
 	client_secret : process.env.client_secret,
-	redirect_uri  : `${process.env.PATH}/api/login/callback`,
+	redirect_uri  : `https://david-hao-tastify.herokuapp.com/api/login/callback`,
 	scopes        : process.env.scopes
 };
