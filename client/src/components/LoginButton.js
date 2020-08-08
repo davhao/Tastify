ecd8d3574b1135ae8188a7c5d40fb97871f99634
David Hao <djhao@live.unc.edu>
2020-08-07 22:28:59 -0400

    typo in a link

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -4,7 +4,7 @@ import { Button } from 'reactstrap';
 class LoginButton extends Component {
 	render() {
 		return (
-			<Button className="main-btn" onClick={(event) => (window.location.href = `${this.props.url}/api/login}`)}>
+			<Button className="main-btn" onClick={(event) => (window.location.href = `${this.props.url}/api/login`)}>
 				Log In With Spotify
 			</Button>
 		);
